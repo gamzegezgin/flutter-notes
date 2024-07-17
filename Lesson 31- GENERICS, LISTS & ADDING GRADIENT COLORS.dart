7b83c37319263926d98f70821cbf62b122959f90
Gamze Gezgin <gamzegezgin@yahoo.com>
2024-07-18 00:10:02 +0300

    Created lesson 31 and 36

---
+++ Lesson 31- GENERICS, LISTS & ADDING GRADIENT COLORS.dart
@@ -0,0 +1,13 @@
+Lesson 31- GENERICS, LISTS & ADDING GRADIENT COLORS
+
+generic types are "flexible types" that "work together" with other types
+
+*in this example List<Color> colors, Color is data type
+
+*here is an example 
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 158, 206, 86),
+            Color.fromARGB(255, 169, 205, 117),
+            Color.fromRGBO(184, 206, 152, 1),
+          ])),
---
+++ Lesson 36- BUILDING CUSTOM WIDGET.dart
@@ -0,0 +1,8 @@
+Lesson 36- BUILDING CUSTOM WIDGET
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return -write content of custom widget-;
+  }
+}
