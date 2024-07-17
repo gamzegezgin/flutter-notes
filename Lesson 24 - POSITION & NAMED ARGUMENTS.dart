e8fef650a6ca98f92a821e52c9c6aeaa5ce421d6
Gamze Gezgin <gamzegezgin@yahoo.com>
2024-07-18 00:01:04 +0300

    I arranged the file

--- Lesson 24 - POSITION & NAMED ARGUMENTS.dart
+++ Lesson 24 - POSITION & NAMED ARGUMENTS.dart
@@ -1,6 +1,5 @@
-Flutter & Dart - The Complete Guide [2024 Edition] - Maximilian Schwarzmuller
 
-24- POSITION & NAMED ARGUMENTS
+Lesson 24- POSITION & NAMED ARGUMENTS
 
 Positional: The position of an argument determines which paramater recevies the value
 
@@ -19,9 +18,6 @@ void add({required a, required b}) // a & b are no longer optional
   print(a + b);
 }
 
-
-
-
 Named: The name of an argument determines which parameter receives the value
 
 void add({a, b}) { // a & b are named parameters (because of the curly braces)
@@ -32,26 +28,3 @@ add(b: 5, a: 10); // 5 is used as a value for b, because it's assigned to that n
 
 *named arguments are optional and can be omitted
 *we could call add() or add(b: 5);
-
-31- Generics, Lists & Adding Gradient Colors
-
-generic types are "flexible types" that "work together" with other types
-
-*in this example List<Color> colors, Color is data type
-
-*here is an example 
-          decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 158, 206, 86),
-            Color.fromARGB(255, 169, 205, 117),
-            Color.fromRGBO(184, 206, 152, 1),
-          ])),
-
-36- Building Custom Widget
-
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return -write content of custom widget-;
-  }
-}
